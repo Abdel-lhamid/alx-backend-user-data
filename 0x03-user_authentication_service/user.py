f20ac46bbb761ed691828cde2008017c253466ad
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-09-25 10:12:11 +0100

    fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """user module"""
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,7 +11,7 @@ Base = declarative_base()
 
 class User(Base):
     """User class"""
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
